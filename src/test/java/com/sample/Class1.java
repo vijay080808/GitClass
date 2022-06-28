c8f34fb8c01edb92697196ca477a7c4c2da5c036
vijay080808 <vijayraj0897@gmail.com>
2022-06-28 16:22:24 +0530

    First Time Push

---
+++ src/test/java/com/sample/Class1.java
@@ -0,0 +1,20 @@
+package com.sample;
+
+public class Class1 {
+	
+private void method1() {
+	System.out.println("1");
+
+}
+
+private void method2() {
+	System.out.println("2");
+
+}
+
+private void method3() {
+	System.out.println("3");
+
+}
+
+}
